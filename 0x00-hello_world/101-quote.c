724c287df79af588c0973a2d4bc3be45fa58ed5d
stitchmunir <askmusty23@gmail.com>
2022-12-11 17:33:43 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
 /**
- * main - and that piece of art is useful
- *
- * Return: Always 1 (Success)
+ * main - Entry point
+ * Return: Always 1
  */
 
 int main(void)
-
 {
 	write(2, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
