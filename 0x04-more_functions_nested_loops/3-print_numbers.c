2790c2540f33ff934c1888e3efc818115c878dd7
stitchmunir <askmusty23@gmail.com>
2022-12-15 16:28:37 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * print_numbers - checks for a digit 0 through 9
- * Return: Always 0
+ * print_numbers - prints 0 through 9
+ *
+ * Return: void
  */
 void print_numbers(void)
 {
